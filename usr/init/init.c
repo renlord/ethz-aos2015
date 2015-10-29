4d95875b765c5da2e59655d73781604ab3eb5e48
Anders Kirk Uhrenholt <akuhren@gmail.com>
2015-10-29 18:32:52 +0100

    init.c: Added recv-function as well as an infinite loop memeater.c: Changed cap_selfep to cap_initep

--- usr/init/init.c
+++ usr/init/init.c
@@ -23,6 +23,8 @@
 struct bootinfo *bi;
 static coreid_t my_core_id;
 
+void recv_handler(void *lc_in);
+
 int main(int argc, char *argv[])
 {
     errval_t err;
@@ -51,7 +53,7 @@ int main(int argc, char *argv[])
         abort();
     }
     debug_printf("initialized local ram alloc\n");
-
+    
     // setup memory serving
     err = initialize_mem_serv();
     if (err_is_fail(err)) {
@@ -59,6 +61,9 @@ int main(int argc, char *argv[])
         abort();
     }
 
+    char *c = (char*)malloc(2);
+    *c = 'c';
+    
     // TODO (milestone 4): Implement a system to manage the device memory
     // that's referenced by the capability in TASKCN_SLOT_IO in the task
     // cnode. Additionally, export the functionality of that system to other
@@ -69,16 +74,61 @@ int main(int argc, char *argv[])
 
     // TODO (milestone 3) STEP 2:
     // get waitset
+    // TODO
+
     // allocate lmp chan
+    struct lmp_chan lc;
+
     // init lmp chan
+    lmp_chan_init(&lc);
+   
     /* make local endpoint available -- this was minted in the kernel in a way
      * such that the buffer is directly after the dispatcher struct and the
      * buffer length corresponds DEFAULT_LMP_BUF_WORDS (excluding the kernel 
      * sentinel word).
      */
+    const uint64_t FIRSTEP_BUFLEN = 21u;
+    const uint64_t FIRSTEP_OFFSET = (33472u + 56u);
+
+    struct lmp_endpoint *my_ep;
+    lmp_endpoint_setup(FIRSTEP_OFFSET, FIRSTEP_BUFLEN, &my_ep);
+    
+    lc.endpoint = my_ep; 
+    lmp_chan_register_recv(&lc, get_default_waitset(),
+        MKCLOSURE(recv_handler, &lc));
+ 
+    printf("next:          0x%08x\n", my_ep->next);
+    printf("prev:          0x%08x\n", my_ep->prev);
+    printf("recv_slot:     0x%08x\n", my_ep->recv_slot);
+    printf("waitset_state: 0x%08x\n", my_ep->waitset_state);
+    printf("buflen:        0x%08x\n", my_ep->buflen);
+    printf("seen:          0x%08x\n", my_ep->seen);
+    printf("k:             0x%08x\n", my_ep->k);
+    
     // allocate slot for incoming capabilites
     // register receive handler 
     // go into messaging main loop
-
+    
+    while(true);
+    
     return EXIT_SUCCESS;
+}
+
+void recv_handler(void *lc_in)
+{
+    struct lmp_chan *lc = lc_in;
+    struct lmp_recv_msg msg = LMP_RECV_MSG_INIT;
+    struct capref cap;
+    errval_t err = lmp_chan_recv(lc, &msg, &cap);
+    if (err_is_fail(err) && lmp_err_is_transient(err)) {
+        // reregister
+        lmp_chan_register_recv(lc, get_default_waitset(),
+            MKCLOSURE(recv_handler, lc));
+        return;
+    }
+
+    debug_printf("msg buflen %zu\n", msg.buf.msglen);
+    debug_printf("msg->words[0] = 0x%lx\n", msg.words[0]);
+    lmp_chan_register_recv(lc, get_default_waitset(),
+        MKCLOSURE(recv_handler, lc_in));
 }
