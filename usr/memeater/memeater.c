6740cb840e9ceff06243906425f3c0e77e18e43f
Anders Kirk Uhrenholt <akuhren@gmail.com>
2015-10-30 02:47:19 +0100

    Message from memeater can be read (actively) in init.

--- usr/memeater/memeater.c
+++ usr/memeater/memeater.c
@@ -13,24 +13,24 @@ int main(int argc, char *argv[])
 {
     debug_printf("memeater started\n");
 
-    const uint64_t FIRSTEP_BUFLEN = 21u;
-    const uint64_t FIRSTEP_OFFSET = (33472u + 56u);
+    uint32_t FIRSTEP_BUFLEN = 21u;
+    // uint32_t FIRSTEP_OFFSET = (33472u + 56u);
 
     struct lmp_chan lc;
     lmp_chan_init(&lc);
 
+    printf("Setting up endpoint.\n");
     struct lmp_endpoint *my_ep;
-    lmp_endpoint_setup(FIRSTEP_OFFSET, FIRSTEP_BUFLEN, &my_ep);
+    lmp_endpoint_setup(0, FIRSTEP_BUFLEN, &my_ep);
     
     lc.endpoint = my_ep;
     lc.remote_cap = cap_initep; 
     
+    printf("Sending message.\n");
     char *buf = "Whatup!";
-    lmp_chan_send(&lc, 1 << 1, NULL_CAP, 8,
+    lmp_chan_send(&lc, LMP_SEND_FLAGS_DEFAULT, NULL_CAP, 8,
                   buf[0], buf[1], buf[2], buf[3], buf[4],
                   buf[5], buf[6], buf[7], buf[8]);       
-    
-    thread_yield_dispatcher(cap_initep);
     
     // errval_t err;
     // // TODO STEP 1: connect & send msg to init using syscall
@@ -50,6 +50,9 @@ int main(int argc, char *argv[])
     //     debug_printf("Message sent...\n");
     // else
     //     debug_printf("Message is NOT sent.");
-    // // TODO STEP 5: test memory allocation using memserv
+    
+    thread_yield_dispatcher(cap_initep);
+
+    // TODO STEP 5: test memory allocation using memserv
     return 0;
 }
