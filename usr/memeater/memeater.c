096824dd354ab2d203a7bc49fee2017f7dd1ec04
renlord <renlord.yang@gmail.com>
2015-10-30 04:15:59 +0100

    skeleton for RPC

--- usr/memeater/memeater.c
+++ usr/memeater/memeater.c
@@ -75,29 +75,27 @@ int main(int argc, char *argv[])
 
     thread_yield_dispatcher(cap_initep);
 
-    buf = "hello world!";
-    lmp_chan_send(&lc, LMP_SEND_FLAGS_DEFAULT, NULL_CAP, 8,
-                  buf[0], buf[1], buf[2], buf[3], buf[4],
-                  buf[5], buf[6], buf[7], buf[8]);     
+    // buf = "hello world!";
+    // lmp_chan_send(&lc, LMP_SEND_FLAGS_DEFAULT, NULL_CAP, 8,
+    //               buf[0], buf[1], buf[2], buf[3], buf[4],
+    //               buf[5], buf[6], buf[7], buf[8]);     
 
-    if (err_is_ok(err)) {
-        debug_printf("part4_2 send successful\n");
-    } else {
-        debug_printf("part4_2 send fail. err:%d\n", err);
-    }
+    // if (err_is_ok(err)) {
+    //     debug_printf("part4_2 send successful\n");
+    // } else {
+    //     debug_printf("part4_2 send fail. err:%d\n", err);
+    // }
 
-    thread_yield_dispatcher(cap_initep);
+    // thread_yield_dispatcher(cap_initep);
 
 
     // part 5. 
-    struct capref to_memeater;
-    struct lmp_endpoint *to_memeater_ep;
-    err = endpoint_create(DEFAULT_LMP_BUF_WORDS, &to_memeater, &to_memeater_ep);
-    if (err_is_fail(err)) {
-        debug_printf("failed to create ep cap. Err: %d\n", err);
+    err = lmp_chan_alloc_recv_slot(&lc);
+    if (err_is_fail(err)){
+        printf("Could not allocate receive slot!\n");
+        exit(-1);
     }
-    //err = lmp_chan_send0(&lc, LMP_SEND_FLAGS_DEFAULT, my_ep->recv_slot);
-    err = lmp_chan_send0(&lc, LMP_SEND_FLAGS_DEFAULT, to_memeater);
+    err = lmp_chan_send0(&lc, LMP_SEND_FLAGS_DEFAULT, my_ep->recv_slot);
     if (err_is_ok(err)) {
         debug_printf("p5 cap send from memeater to init success.\n");
     } else {
